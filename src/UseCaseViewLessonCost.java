c8ccd79dc402399289b24798b7f7c7de65e500bd
ymkschoolpc <1yanmyokyaw@gmail.com>
2026-02-10 14:42:06 +0900

    refactor

--- src/UseCaseViewLessonCost.java
+++ src/UseCaseViewLessonCost.java
@@ -1,4 +1,4 @@
-public class ViewLessonCost {
+public class UseCaseViewLessonCost {
     public static void execute() {
         System.out.println("\n------ 単価 ------");
         System.out.println("レッスン単価=" + LessonCost.getLessonCost() + "ポイント");
